f2b3571034eae5390187fbe485b12abcca52370f
LayBraid <clement.loeuillet@epitech.eu>
2022-02-12 02:20:30 +0100

    add reader json

--- main.lua
+++ main.lua
@@ -2,14 +2,27 @@
     Your love2d game start here
 ]]
 
+local json = require("dkjson")
+
+step = 1
+mapDec = {}
 
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
 function love.load()
-    -- init something here ...
-    love.window.setTitle('Hello love2d!')
+    love.window.setTitle('FAB')
 
     love.keyboard.keysPressed = {}
+
+    mapJson = love.filesystem.read("dico.json")
+
+    mapDec = json.decode(mapJson)
+
+    for i, texture in ipairs(mapDec.categories) do
+    		print(texture.name)
+    		print("---")
+    	end
+
 end
 
 function love.resize(w, h)
@@ -35,7 +48,10 @@ function love.update(dt)
 end
 
 function love.draw()
-    -- draw your stuff here
-    love.graphics.print('Welcome to the Love2d world!', 10, 10)
+    if step == 0 then
+        love.graphics.print('Welcome to the Love2d world!', 10, 10)
+    else
+        love.graphics.print('1', 10, 10)
+    end
 end
 
